414b1698d3360b19513b6f51246a5efe1134bd54
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2024-12-11 00:43:06 +0530

    viewmodel setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 dependencies {
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation ("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
 
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
--- app/src/main/java/com/example/locationbasics/LocationUtils.kt
+++ app/src/main/java/com/example/locationbasics/LocationUtils.kt
@@ -3,12 +3,34 @@ package com.example.locationbasics
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Looper
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class LocationUtils(val context: Context) {
 
+    private val _fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
 
+    fun requestLocationUpdates(viewModel: LocationVieModel){
+        val locationCallBack = object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                locationResult.lastLocation?.let {
+                    val location = LocationData(latitude = it.latitude, longitude = it.longitude)
+                    viewModel.updateLocation(location)
+                }
+            }
+        }
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,1000).build()
 
+        _fusedLocationClient.requestLocationUpdates(locationRequest,locationCallBack, Looper.getMainLooper())
+    }
 
     fun hasLocationPermission(context: Context): Boolean{
 
