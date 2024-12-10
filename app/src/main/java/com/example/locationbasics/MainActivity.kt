ca7abd462107f2caa8a10b84ab8f3017eb61bfad
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2024-12-10 13:22:55 +0530

    permissions

---
+++ app/src/main/java/com/example/locationbasics/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.locationbasics
+
+import android.Manifest
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import com.example.locationbasics.ui.theme.LocationBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LocationBasicsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(){
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+    LocationDisplay(locationUtils = locationUtils, context = context)
+}
+
+@Composable
+fun LocationDisplay(
+    locationUtils: LocationUtils,
+    context: Context
+){
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestMultiplePermissions(),
+            onResult = {permissions ->
+                if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+                    &&
+                    permissions[Manifest.permission.ACCESS_COARSE_LOCATION]== true){
+
+                }else{
+                    //request location permission
+                    val rationaleRequired = ActivityCompat.shouldShowRequestPermissionRationale(
+                        context as MainActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                        context,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+
+                    if (rationaleRequired){
+                        Toast.makeText(context,
+                            "Location Permission is required",
+                            Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(context,
+                            "Location Permission is required. Enable in Settings",
+                            Toast.LENGTH_LONG).show()
+                    }
+                }
+        })
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Location not available")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)){
+                //permission already granted
+            }else{
+                requestPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                )
+            }
+        }) {
+            Text(text = "Get Location")
+        }
+    }
+}
